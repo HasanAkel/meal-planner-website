4e1c0c747bfd53056f84854a01d997880e520131
Ahmed-s-13 <superada4@gmail.com>
2025-11-14 16:07:21 +0400

    all files now work with eclipse

--- js/tracker.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.time-filter-btn').forEach(button => {button.addEventListener('click',function() {
-	document.querySelectorAll('.time-filter-btn').forEach(button => button.classList.remove('active'));
-	this.classList.add('active');
-	//this should then update the displayed data with regards to the selected time when fully implemented
-	});
-});
